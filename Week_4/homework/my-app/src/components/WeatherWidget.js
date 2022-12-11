5da95bbb918612c3b642e54944de346c0a3231af
Paul Delseith <pauldelseith@wireless-10-148-9-44.public.utexas.edu>
2022-12-11 01:30:30 -0600

    Added prototype weather cards

--- Week_4/homework/my-app/src/components/WeatherWidget.js
+++ Week_4/homework/my-app/src/components/WeatherWidget.js
@@ -2,9 +2,29 @@ import React from "react";
 import Button from "react-bootstrap/esm/Button";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/esm/Card'
 
 export default function WeatherWidget() {
     return (
-    <img src = "/icons/01d.svg" alt="My Happy SVG"/>
-    );
+    <div className="row-weather">
+    <Card className="weather-widget">
+        <div className="card-body">
+            <h5 className="card-title-weather">Monday</h5>
+            <img src = "/icons/01d.svg" alt="My Happy SVG"/>
+        </div> 
+    </Card>
+    <Card className="weather-widget">
+        <div className="card-body">
+            <h5 className="card-title-weather">Monday</h5>
+            <img src = "/icons/01d.svg" alt="My Happy SVG"/>
+        </div>
+    </Card>
+    <Card className="weather-widget">
+        <div className="card-body">
+            <h5 className="card-title-weather">Monday</h5>
+            <img src = "/icons/01d.svg" alt="My Happy SVG"/>
+        </div>
+    </Card>
+    </div>
+    ); 
 }
